d2b5527001f1e2ce9cdf588308ae9f8b58f056c9
Victor Emanuel <victorers2@gmail.com>
2020-05-12 22:12:43 -0300

    feat(factory): working on Javascript implementation

--- Factory/Javascript/index.js
+++ Factory/Javascript/index.js
@@ -1,1 +1,26 @@
 console.log(`hello from index.js`);
+
+class Car {
+
+}
+
+class Gol extends Car {
+
+}
+
+class Amarok extends Car {
+
+}
+
+class Fox extends Car {
+
+}
+
+function createCar(model) {
+    const supportedCars = {
+        Gol,
+        Amarok,
+        Fox
+    }
+    return new (supportedCars[model] || Car)();
+}
