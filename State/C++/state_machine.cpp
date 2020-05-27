53e86718fdb43ae306efde32d447f2f7e8994d30
Victor Emanuel <victorers2@gmail.com>
2020-05-26 22:34:03 -0300

    feat(state): starts c++ implementation of state pattern

--- State/C++/state_machine.cpp
+++ State/C++/state_machine.cpp
@@ -2,11 +2,60 @@
 
 using namespace std;
 
-class State {
-
+/**
+ * List of states
+ * Q0: even 0, even 1
+ * Q1: odd 0, odd 1
+ * Q2: odd 0, even 1
+ * Q3: even 0, odd 1
+ */
+enum State
+{
+    kQ0,
+    kQ1,
+    kQ2,
+    kQ3
 };
 
+class StateInterface
+{
+public:
+    virtual void ChangeState(char digit) {}
 
+private:
+    State state_;
+};
+
+/**
+ * Initial state
+ * Even 0, even 1
+ */
+class State0 : public StateInterface
+{
+public:
+    State0()
+    {
+    }
+};
+
+/**
+ * Context Class
+ * 
+ * This machine identifies if a binary string has 
+ * a odd number of 0s and even number of 1s
+ */
+class Machine
+{
+    class StateInterface *current_;
+
+public:
+    Machine();
+
+    void SetCurrentState(StateInterface *state)
+    {
+        current_ = state;
+    }
+};
 
 int main()
 {
